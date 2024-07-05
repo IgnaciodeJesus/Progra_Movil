7f3978ea4f2466c25c48ec29905c8e74ace9eaed
yomero21 <20191529@aloe.ulima.edu.pe>
2024-07-05 05:49:54 -0500

    sign_up+passw funcional
    la pagina sign up y password recovery funcional conectado con el backend + control de errores.

--- cineulima/lib/pages/recover/password_recovery_controller.dart
+++ cineulima/lib/pages/recover/password_recovery_controller.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/services.dart'; // Importa este paquete para acceder al portapapeles
 import '../../configs/constants.dart';
 import '../../models/entities/Usuario.dart';
 
@@ -9,6 +12,7 @@ class PasswordRecoveryController extends GetxController {
 
   var dni = ''.obs;
   var correo = ''.obs;
+  var nuevaContrasena = ''.obs; // Observador para almacenar la nueva contraseña
 
   @override
   void onInit() {
@@ -27,7 +31,7 @@ class PasswordRecoveryController extends GetxController {
     super.onClose();
   }
 
-  void recoverPassword() {
+  void recoverPassword() async {
     if (dni.value.isEmpty || correo.value.isEmpty) {
       Get.snackbar(
         'Error',
@@ -37,39 +41,101 @@ class PasswordRecoveryController extends GetxController {
       return;
     }
 
-    // Verificar si el DNI y el correo están registrados
-    bool usuarioEncontrado = false;
-
-    for (var usuario in USUARIOS) {
-      if (usuario.dni == dni.value && usuario.correo == correo.value) {
-        usuarioEncontrado = true;
-        break;
-      }
-    }
-
-    if (usuarioEncontrado) {
-      // Limpiar los campos de texto después de recuperar la contraseña
-      dniController.clear();
-      correoController.clear();
-
-      Get.defaultDialog(
-        title: "Recuperar Contraseña",
-        middleText:
-            "Un correo electrónico de restablecimiento de contraseña ha sido enviado a ${correo.value}. Por favor, revisa tu bandeja de entrada y sigue las instrucciones.",
-        textConfirm: "Aceptar",
-        onConfirm: () {
-          Get.back(); // Cerrar el diálogo
-          Get.toNamed('/login'); // Navegar a la página de inicio de sesión
+    try {
+      final response = await http.post(
+        Uri.parse(
+            'http://192.168.0.138:4567/user/reset'), // Usa tu IP y puerto correctos
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
         },
+        body: jsonEncode(<String, String>{
+          'dni': dni.value,
+          'correo': correo.value,
+        }),
       );
-    } else {
+
+      final responseBody = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        nuevaContrasena.value = responseBody['message'].replaceAll(
+            'Contraseña actualizada: ', ''); // Extrae solo la contraseña
+        Get.defaultDialog(
+          title: "Recuperar Contraseña",
+          middleText: "",
+          content: Column(
+            children: [
+              SizedBox(height: 10),
+              Text(
+                "Has cambiado tu contraseña con éxito : ",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                nuevaContrasena.value,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  Clipboard.setData(ClipboardData(text: nuevaContrasena.value));
+                  Get.snackbar(
+                    'Copiado',
+                    '¡Contraseña copiada al portapapeles!',
+                    snackPosition: SnackPosition.BOTTOM,
+                    backgroundColor: Colors.green,
+                    colorText: Colors.white,
+                  );
+                  Get.back(); // Cerrar el diálogo después de copiar
+                  Get.toNamed('/login'); // Redirigir al login
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color:
+                        Colors.blueAccent, // Color de fondo llamativo del botón
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    'Copiar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+        // Limpiar los campos de texto después de recuperar la contraseña
+        dniController.clear();
+        correoController.clear();
+      } else {
+        Get.snackbar(
+          'Error',
+          responseBody['message'],
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+      }
+    } catch (e) {
       Get.snackbar(
         'Error',
-        'No se encontró ningún usuario con el DNI y correo electrónico proporcionados.',
+        'Ocurrió un error al intentar recuperar la contraseña.',
         snackPosition: SnackPosition.BOTTOM,
         backgroundColor: Colors.red,
         colorText: Colors.white,
       );
+      print('Error inesperado: $e');
     }
   }
 }
--- cineulima/lib/pages/signup/sign_up_controller.dart
+++ cineulima/lib/pages/signup/sign_up_controller.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:cineulima/models/entities/Usuario.dart';
 import 'package:cineulima/configs/constants.dart';
 
@@ -27,12 +29,12 @@ class SignUpController extends GetxController {
     correoController = TextEditingController();
     passwordController = TextEditingController();
     confirmPasswordController = TextEditingController();
-    nombre.value="";
-    apellido.value="";
-    dni.value="";
-    correo.value="";
-    password.value="";
-    confirmPassword.value="";
+    nombre.value = "";
+    apellido.value = "";
+    dni.value = "";
+    correo.value = "";
+    password.value = "";
+    confirmPassword.value = "";
   }
 
   @override
@@ -43,11 +45,10 @@ class SignUpController extends GetxController {
     correoController.dispose();
     passwordController.dispose();
     confirmPasswordController.dispose();
-
     super.onClose();
   }
 
-  void createAccount() {
+  void createAccount() async {
     if (nombre.value.isEmpty ||
         apellido.value.isEmpty ||
         dni.value.isEmpty ||
@@ -58,6 +59,8 @@ class SignUpController extends GetxController {
         'Error',
         'Por favor, complete todos los campos.',
         snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
       );
       return;
     }
@@ -67,39 +70,77 @@ class SignUpController extends GetxController {
         'Error',
         'Las contraseñas no coinciden.',
         snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
       );
       return;
     }
 
-    Usuario nuevoUsuario = Usuario(
-      id: USUARIOS.length + 1,
-      nombre: nombre.value,
-      apellido: apellido.value,
-      dni: dni.value,
-      correo: correo.value,
-      password: password.value,
-      fotoPerfil: '',
-    );
+    try {
+      final response = await http.post(
+        Uri.parse(
+            'http://192.168.0.138:4567/user/create'), // Usa tu IP y puerto correctos
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'nombre': nombre.value,
+          'apellido': apellido.value,
+          'dni': dni.value,
+          'correo': correo.value,
+          'password': password.value,
+          'foto_perfil': '',
+        }),
+      );
 
-    // Agregar el nuevo usuario a la lista observable de usuarios
-    USUARIOS.add(nuevoUsuario);
+      if (response.statusCode == 200) {
+        Get.defaultDialog(
+          title: "Registro Exitoso",
+          middleText: "¡Te has registrado correctamente!",
+          textConfirm: "Aceptar",
+          onConfirm: () {
+            Get.back();
+            Get.toNamed('/login');
+          },
+        );
 
-    Get.defaultDialog(
-      title: "Registro Exitoso",
-      middleText: "¡Te has registrado correctamente!",
-      textConfirm: "Aceptar",
-      onConfirm: () {
-        Get.back();
-        Get.toNamed('/login');
-      },
-    );
-
-    // Limpiar los campos de entrada
-    nombreController.clear();
-    apellidoController.clear();
-    dniController.clear();
-    correoController.clear();
-    passwordController.clear();
-    confirmPasswordController.clear();
+        // Limpiar los campos de entrada
+        nombreController.clear();
+        apellidoController.clear();
+        dniController.clear();
+        correoController.clear();
+        passwordController.clear();
+        confirmPasswordController.clear();
+      } else {
+        final responseBody = jsonDecode(response.body);
+        Get.snackbar(
+          'Error',
+          responseBody['message'],
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+        print('Error: ${responseBody['message']}');
+      }
+    } on http.ClientException catch (e) {
+      Get.snackbar(
+        'Error de Conexión',
+        'No se pudo conectar con el servidor: ${e.message}',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+      print(
+          'Error de Conexión: No se pudo conectar con el servidor: ${e.message}');
+    } catch (e) {
+      Get.snackbar(
+        'Error inesperado',
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+      print('Error inesperado: $e');
+    }
   }
 }
